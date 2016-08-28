bcb08b98912fa5fba42ab55d4cfe9129a1980416
Rahul Krishnan <rk2211@gmail.com>
2016-08-28 06:41:22 -0400

    Continued Unit 4

---
+++ crypt_combining/code.rb
@@ -0,0 +1,15 @@
+crypt_of_civilization = []
+
+crypt_of_civilization << "Comptometer"
+crypt_of_civilization << "Box of phonographic records"
+crypt_of_civilization << "Plastic savings bank"
+crypt_of_civilization << "Set of scales"
+crypt_of_civilization << "Toast-O-Lator"
+crypt_of_civilization << "Sample of aluminum foil"
+crypt_of_civilization << "Donald Duck doll"
+
+extra = ["Container of beer", "Lionel model train set", "Ingraham pocket watch"]
+
+crypt_of_civilization = crypt_of_civilization + extra
+
+puts crypt_of_civilization
---
+++ crypt_filling/code.rb
@@ -0,0 +1,11 @@
+crypt_of_civilization = []
+
+crypt_of_civilization << "Comptometer"
+crypt_of_civilization << "Box of phonographic records"
+crypt_of_civilization << "Plastic savings bank"
+crypt_of_civilization << "Set of scales"
+crypt_of_civilization << "Toast-O-Lator"
+crypt_of_civilization << "Sample of aluminum foil"
+crypt_of_civilization << "Donald Duck doll"
+
+puts crypt_of_civilization
