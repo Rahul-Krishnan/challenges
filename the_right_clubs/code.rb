5dc97d36eb99e5262135bd65949b5d4f5c410f12
Rahul Krishnan <rk2211@gmail.com>
2016-08-29 03:02:56 -0400

    Started Unit 5

--- the_right_clubs/code.rb
+++ the_right_clubs/code.rb
@@ -1,5 +1,11 @@
 REQUIRED_BAG_SIZE = 10
 
+required_clubs = [
+  :driver,
+  :pitching_wedge,
+  :putter
+]
+
 available_clubs = [
   :two_iron,
   :three_iron,
@@ -21,3 +27,15 @@ available_clubs = [
 possible_club_selections = available_clubs.sample(REQUIRED_BAG_SIZE)
 
 # YOUR CODE GOES BELOW
+puts "Here's what the robot caddy thinks:"
+puts ""
+possible_club_selections.each do |item|
+  puts "Try the #{item.to_s.gsub("_", " ")}."
+end
+
+puts ""
+required_clubs.each do |item|
+  unless possible_club_selections.include?(item)
+    puts "*** WARNING! You're missing a #{item.to_s.tr("_", " ")} ***"
+  end
+end
