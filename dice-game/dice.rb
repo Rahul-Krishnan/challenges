df316700922fb24189c0e47245d1377d097a45fa
Rahul Krishnan <rk2211@gmail.com>
2016-10-04 23:32:21 -0400

    fixed simple dice game

--- dice-game/dice.rb
+++ dice-game/dice.rb
@@ -1,38 +1,25 @@
 # YOUR CODE, HERE
-def roll_die(sides)
-  result = rand(1..sides)
+def roll_die
+  result = rand(1..6)
   return result
 end
 
-def play (n, sides)
-  n.times do
-    die_1 = roll_die(sides)
-    die_2 = roll_die(sides)
+def play
+  2.times do
+    die_1 = roll_die
+    die_2 = roll_die
     sum = die_1 + die_2
     puts "Total: #{sum}\tYou rolled a #{die_1} and a #{die_2}."
   end
 end
 
 system "clear"
-puts "\n*** Welcome to the local alley dice game! ***"
+puts "\n*** Welcome to the local alley dice game! ***\n\n"
 
 quit = 0
 while quit != "q"
 
-  faces = 0
-  while faces == 0
-    puts "\n\nHow many sides to your dice?"
-    faces = gets.chomp.to_i
-  end
-
-  rolls = 0
-  while rolls == 0
-    puts "\n\nHow many rolls would you like?"
-    rolls = gets.chomp.to_i
-    puts
-  end
-
-  play(rolls, faces)
+  play
 
   puts "\n\nPress Q to quit or any other key to continue..."
   quit = gets.chomp.downcase
