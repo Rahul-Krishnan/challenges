28bb53d9bd65c5cd1f90d082dc6deb0b8e78067d
Rahul Krishnan <rk2211@gmail.com>
2016-11-18 19:28:14 -0500

    Completed Automat and Todo JS exercises

--- automat-js/main.js
+++ automat-js/main.js
@@ -1,1 +1,14 @@
 // your code, here
+let main = document.getElementById("main");
+
+let menu = ["Chicken Pot Pie", "Baked Beans", "Macaroni and Cheese", "Burgundy Sauce with Beef and Noodles", "Creamed Spinach", "Pumpkin Pie", "Strawberry Shortcake"];
+
+for (let i = 0; i < menu.length; i++) {
+  let foodItemDiv = document.createElement('div');
+  foodItemDiv.className = 'food-item';
+  foodItemDiv.innerHTML = menu[i];
+  foodItemDiv.addEventListener('click', () => {
+    foodItemDiv.className += ' hidden';
+  });
+  main.appendChild(foodItemDiv);
+}
--- todo-js/main.js
+++ todo-js/main.js
@@ -1,1 +1,22 @@
 // your code, here
+let todo = ['Write some Javascript', 'Submit System Check', 'Take a break', 'Watch Dr. Strange', 'Visit friends', 'Feed guinea pigs'];
+
+let listDiv = document.getElementById('todo-list');
+
+for (let i = 0; i < todo.length; i++) {
+  let itemDiv = document.createElement('li');
+  itemDiv.innerHTML = todo[i];
+  listDiv.appendChild(itemDiv);
+}
+
+let inputTodoItem = document.getElementById('new-todo-item');
+
+inputTodoItem.addEventListener('keyup', (event) => {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    let newItem = document.createElement('li');
+    newItem.innerHTML = inputTodoItem.value;
+    listDiv.appendChild(newItem);
+    inputTodoItem.value = "";
+  }
+});
