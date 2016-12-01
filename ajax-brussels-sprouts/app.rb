3cc864cbee1ce9820565a2f49c47de7105f71e7b
Rahul Krishnan <rk2211@gmail.com>
2016-11-30 23:22:15 -0500

    Working Ajax Brussels Sprouts

--- ajax-brussels-sprouts/app.rb
+++ ajax-brussels-sprouts/app.rb
@@ -8,19 +8,22 @@ configure do
   set :public_folder, 'public'
 end
 
-dishes = [
-  "Roasted Brussels Sprouts",
-  "Fresh Brussels Sprouts Soup",
-  "Brussels Sprouts with Toasted Breadcrumbs, Parmesan, and Lemon",
-  "Cheesy Maple Roasted Brussels Sprouts and Broccoli with Dried Cherries",
-  "Hot Cheesy Roasted Brussels Sprout Dip",
-  "Pomegranate Roasted Brussels Sprouts with Red Grapes and Farro",
-  "Roasted Brussels Sprout and Red Potato Salad",
-  "Smoky Buttered Brussels Sprouts",
-  "Sweet and Spicy Roasted Brussels Sprouts",
-  "Smoky Buttered Brussels Sprouts",
-  "Brussels Sprouts and Egg Salad with Hazelnuts"
-]
+def sample_dish
+  dishes = [
+    "Roasted Brussels Sprouts",
+    "Fresh Brussels Sprouts Soup",
+    "Brussels Sprouts with Toasted Breadcrumbs, Parmesan, and Lemon",
+    "Cheesy Maple Roasted Brussels Sprouts and Broccoli with Dried Cherries",
+    "Hot Cheesy Roasted Brussels Sprout Dip",
+    "Pomegranate Roasted Brussels Sprouts with Red Grapes and Farro",
+    "Roasted Brussels Sprout and Red Potato Salad",
+    "Smoky Buttered Brussels Sprouts",
+    "Sweet and Spicy Roasted Brussels Sprouts",
+    "Smoky Buttered Brussels Sprouts",
+    "Brussels Sprouts and Egg Salad with Hazelnuts"
+  ]
+  dishes.sample
+end
 
 get '/' do
   erb :index
@@ -28,6 +31,6 @@ end
 
 get '/dishes/random.json' do
   content_type :json
-  dishes.sample
+  { dish: sample_dish }.to_json
   # serve up a random dish from the `dishes` array
 end
--- ajax-brussels-sprouts/public/javascripts/app.js
+++ ajax-brussels-sprouts/public/javascripts/app.js
@@ -1,8 +1,7 @@
 $('#get-dish').on('click', function(event) {
   event.preventDefault();
-  $.get("/dishes/random.json", function(response) {
-    alert(response.content);
+  $.get("/dishes/random.json", function(sample_dish) {
+    alert("Your dish is " + sample_dish["dish"]);
   });
-  debugger;
   // request a new dish and alert the user
 });
