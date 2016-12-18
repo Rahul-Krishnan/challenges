1bdbcd2e8e6749db3caeeb1c1531f4e3773992e9
Rahul Krishnan <rk2211@gmail.com>
2016-12-18 18:49:24 -0500

    Enabled React and FontAwesome icon, enabled basic functionality

--- pop-up-ad/src/main.js
+++ pop-up-ad/src/main.js
@@ -3,11 +3,28 @@ import './app.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let headerElement = React.createElement(
-  'h1'
-);
+
+let divElement =
+<div>
+  <div onClick={event => confirm("Are you sure?")}>
+    <i className="fa fa-times" aria-hidden="true"></i>
+  </div>
+
+  <h1 id="pop-up" className="outer-box">
+  You have won tickets to see Taylor Swift!
+  </h1>
+
+  <h2>
+    Please enter your email so we can send you the tickets
+  </h2>
+
+  <form>
+    <input type="text" value="Your email here"/>
+    <input type="submit" value="Claim your prize!"/>
+  </form>
+</div>
 
 ReactDOM.render(
-  headerElement,
+  divElement,
   document.getElementById('app')
 );
