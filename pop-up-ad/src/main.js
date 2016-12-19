42a80dc0aa8a5cc0fe92fb7001c3e93cd826cd40
Rahul Krishnan <rk2211@gmail.com>
2016-12-18 23:02:06 -0500

    added styling to form
    Still needs:
    - black border
    - preventDefault when submitting form
    - add extra shadow to submit box

--- pop-up-ad/src/main.js
+++ pop-up-ad/src/main.js
@@ -4,24 +4,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-let divElement =
-<div>
-  <div onClick={event => confirm("Are you sure?")}>
-    <i className="fa fa-times" aria-hidden="true"></i>
+let divElement = <div>
+  <div className="jumbotron">
+    <div onClick={event => confirm("Are you sure?")}>
+      <i className="fa fa-times fa-lg" aria-hidden="true"></i>
+    </div>
+
+    <h1 id="pop-up" className="outer-box">
+    You have won tickets to see Taylor Swift!
+    </h1>
+
+    <h2>
+      Please enter your email so we can send you the tickets
+    </h2>
+
+    <form id="form">
+      <input type="text" defaultValue="Your email here"/>
+      <input type="submit" value="Claim your prize!"/>
+    </form>
   </div>
-
-  <h1 id="pop-up" className="outer-box">
-  You have won tickets to see Taylor Swift!
-  </h1>
-
-  <h2>
-    Please enter your email so we can send you the tickets
-  </h2>
-
-  <form>
-    <input type="text" value="Your email here"/>
-    <input type="submit" value="Claim your prize!"/>
-  </form>
 </div>
 
 ReactDOM.render(
