7b11b10438d54e336c7c6a535b503c770cdd5cc0
Rahul Krishnan <rk2211@gmail.com>
2016-12-22 20:19:59 -0500

    Managed to get Fetch to work in simple case

--- react-notes-persisted/src/components/App.js
+++ react-notes-persisted/src/components/App.js
@@ -6,10 +6,11 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      answer: ""
     };
   }
   componentDidMount() {
-    let location = 'http://www.google.com/#q=hello';
+    let location = 'http://localhost:4567/folders/1/notes.json';
     console.log("mounted");
     fetch(location)
     .then(response => {
@@ -24,6 +25,9 @@ class App extends Component {
     .then(response => response.json())
     .then(body => {
       console.log(body);
+      let newAnswer = body.notes[0].body;
+      console.log(newAnswer);
+      this.setState({ answer: newAnswer });
     })
     .catch(error => console.error("Error in fetch, you dummy!"));
   }
@@ -32,7 +36,8 @@ class App extends Component {
     console.log("rendering");
     return (
       <div>
-        <h1>Testing, one two three</h1>
+        <h1>Testing, one two three:</h1>
+        <h3>{this.state.answer}</h3>
       </div>
     )
   }
