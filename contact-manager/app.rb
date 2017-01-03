c3a18bf4961413be3bafa3443fcd805fddede030
Rahul Krishnan <rk2211@gmail.com>
2017-01-03 12:40:49 -0500

    Downloaded Meetups in Space exercise

--- contact-manager/app.rb
+++ contact-manager/app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require 'pry'
 
 require_relative 'models/contact'
 also_reload 'models/contact'
@@ -8,6 +9,7 @@ also_reload 'models/contact'
 set :bind, '0.0.0.0'  # bind to all interfaces
 
 get '/' do
+  binding.pry
   @contacts = Contact.all
   erb :index
 end
