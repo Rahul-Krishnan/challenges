63b23ff06af9f39b8dd7dfece29aa35d3081be8a
Rahul Krishnan <rk2211@gmail.com>
2017-01-03 16:29:18 -0500

    Added team dunks vs starbucks

--- meetups-in-space/app.rb
+++ meetups-in-space/app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require 'sinatra'
 require_relative 'config/application'
 
 set :bind, '0.0.0.0'  # bind to all interfaces
--- meetups-in-space/db/schema.rb
+++ meetups-in-space/db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140606200404) do
+ActiveRecord::Schema.define(version: 20170103181552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meetings", force: :cascade do |t|
+    t.string   "mid",          null: false
+    t.string   "meeting_name", null: false
+    t.string   "description",  null: false
+    t.string   "location",     null: false
+    t.string   "creator_name", null: false
+    t.string   "creator_id",   null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "meetings", ["mid", "creator_id"], name: "index_meetings_on_mid_and_creator_id", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",   null: false
