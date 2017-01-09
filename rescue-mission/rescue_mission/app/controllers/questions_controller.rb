fb6df985e2cfe9a2e721d4b6a626cb075b697165
Rahul Krishnan <rk2211@gmail.com>
2017-01-09 14:10:08 -0500

    Added Questions Form

--- rescue-mission/rescue_mission/app/controllers/questions_controller.rb
+++ rescue-mission/rescue_mission/app/controllers/questions_controller.rb
@@ -7,4 +7,24 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question, notice: 'Question was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :descriptions)
+  end
+
+
 end
