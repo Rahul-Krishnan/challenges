45bc1b94dc1a385955a5d3dd1c5f7bf2bf342dba
Rahul Krishnan <rk2211@gmail.com>
2017-01-10 17:57:56 -0500

    Added Fast Food Reviews

--- rescue-mission/rescue_mission/app/controllers/answers_controller.rb
+++ rescue-mission/rescue_mission/app/controllers/answers_controller.rb
@@ -1,23 +1,31 @@
 class AnswersController < ApplicationController
   def index
-    @answers = Answer.all
-  end
-
-  def show
-    @answer = Answer.find(params[:question_id])
+    @question = Question.find(params[:question_id])
+    @answers = @question.answers
   end
 
 
   def new
+    @question = Question.find(params[:question_id])
     @answer = Answer.new
   end
 
   def create
+    @question = Question.find(params[:question_id])
     @answer = Answer.new(answer_params)
+    @answer.question = @question
+
     if @answer.save
-      redirect_to @question, notice: 'Answer was successfully created.'
+      flash[:notice] = 'Answer was successfully created.'
     else
-      render :new
+      flash[:notice] = "Failed to save answer."
     end
+    redirect_to question_path @question
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:answer_description)
   end
 end
