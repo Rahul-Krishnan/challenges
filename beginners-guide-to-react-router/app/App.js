595e6b1651ef9cd434cfd6de863660c6313fedd2
Rahul Krishnan <rk2211@gmail.com>
2017-02-01 10:57:39 -0500

    Started React Router exercise

---
+++ beginners-guide-to-react-router/app/App.js
@@ -0,0 +1,7 @@
+/* jshint esversion: 6 */
+import React, { Component } from 'react'
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
+
+const App = () => <h1>Hello World!</h1>
+
+export default App
