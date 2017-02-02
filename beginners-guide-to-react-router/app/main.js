0b4098d542b8b0e9a6b3d82e0cea2bbe82cd8776
Rahul Krishnan <rk2211@gmail.com>
2017-02-02 16:39:32 -0500

    React Router exercise

--- beginners-guide-to-react-router/app/main.js
+++ beginners-guide-to-react-router/app/main.js
@@ -1,5 +1,5 @@
 /* jshint esversion: 6 */
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-ReactDOM.render(<App />, document.getElementById('root'))
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+ReactDOM.render(<App />, document.getElementById('root'));
