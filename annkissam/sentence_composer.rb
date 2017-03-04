becbe01131409085e097871709b83344bbacd7ae
Rahul Krishnan <rk2211@gmail.com>
2017-03-04 02:42:55 -0500

    solved!

--- annkissam/sentence_composer.rb
+++ annkissam/sentence_composer.rb
@@ -2,12 +2,10 @@
 # sentence must have all valid words
 # sentence must have at least 1 verb
 # sentence have at least 1 noun or at least 2 articles
-
+#
+# Plan:
 # First make a list of all possible word combos
 # Then apply sentence structure rules to filter bad sentences out
-
-string1 = ["abcdefg"]
-string2 = ["abcd"]
 
 def compose_sentences(text)
   nouns = ["abcd", "c", "def", "h", "ij", "cde"]
@@ -16,7 +14,6 @@ def compose_sentences(text)
 
   dictionary = nouns + verbs + articles
   all_sentences = find_all_word_combos(cleanup(text), dictionary)
-  binding.pry
   grammar_check(all_sentences, nouns, verbs, articles)
 end
 
@@ -31,15 +28,18 @@ def find_all_word_combos(string, dictionary)
     first_word = string[0..n]
     if dictionary.include?(first_word)
       sentence = ""
-      sentence += string.slice!(0..n) + " "
-      if string == ""
+      sentence += string[0..n] + " "
+      end_piece = string[(n+1)..(length-1)]
+      if end_piece == ""
+        sentence_list << sentence
+      elsif dictionary.include?(end_piece)
+        sentence += end_piece
         sentence_list << sentence
       else
-        sub_sentence_list = find_all_word_combos(string, dictionary)
+        sub_sentence_list = find_all_word_combos(end_piece, dictionary)
         sub_sentence_list.each do |sub_sentence|
           sub_sentence.prepend(sentence)
         end
-        # binding.pry
         sentence_list += sub_sentence_list
       end
     end
@@ -48,13 +48,15 @@ def find_all_word_combos(string, dictionary)
 end
 
 def grammar_check(sentence_list, nouns, verbs, articles)
+  approved_sentence_list = []
   sentence_list.each do |sentence|
+    sentence.strip!
     if word_check(sentence, verbs, 1) && (word_check(sentence, nouns, 1) || word_check(sentence, articles, 2))
+      approved_sentence_list << sentence
     else
-      sentence_list.delete(sentence)
     end
   end
-  sentence_list
+  approved_sentence_list
 end
 
 def word_check(sentence, word_array, min_count)
